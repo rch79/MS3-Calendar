d630acb743b67f04ebf2839daacc22407ffb1e64
rch79 <rhorta@gmail.com>
2021-09-16 21:53:42 +0000

    Replace get_year_from_user and get_month_from_user functions with get_date_from_user function

--- run.py
+++ run.py
@@ -10,8 +10,14 @@ from google.oauth2.service_account import Credentials
 
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 SERVICE_ACCOUNT_FILE = 'creds.json'
-CREDS = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+CREDS = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
+                                              scopes=SCOPES)
 CALENDAR = GoogleCalendar("13mu09pc1s201mq40c0e51uics@group.calendar.google.com", credentials=CREDS)
+
+month_dictionary = {1: "January", 2: "February", 3: "March", 4: "April",
+                    5: "May", 6: "June", 7: "July", 8: "August",
+                    9: "September", 10: "October", 11: "November",
+                    12: "December"}
 
 
 def display_main_menu():
@@ -29,10 +35,9 @@ def display_main_menu():
     print("* 6. Add event participants        *")
     print("* 7. Remove event participants     *")
     print("* 8. Clear calendar                *")
-    print("* 84. Test get_year_function       *")
-    print("* 85. Test get_month function      *")
-    print("* 87. Test get_time function       *")
-    print("* 88. Add test event               *")
+    print("* 84. Test get_date function       *")
+    print("* 85. Test get_time function       *")
+    print("* 86. Add test event               *")
     print("* 9. Quit                          *")
     print("************************************\n")
 
@@ -42,12 +47,15 @@ def get_user_menu_option():
     Get user selection of one of the main menu options
     Returns an error message if user makes an invalid selection
     """
-    menu_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 84, 85, 86, 87, 88]
+    menu_options = [1, 2, 3, 4, 5, 6, 7, 8, 9, 84, 85, 86]
     user_selection = ""
 
     while user_selection not in menu_options:
         try:
-            user_selection = int(input('Please type your selection (1 - 9): \n'))
+            user_selection = int(input(
+                                        "Please type your "
+                                        "selection (1 - 9): \n"
+                                        ))
             if user_selection not in menu_options:
                 print("Please select a valid menu option")
         except ValueError:
@@ -64,16 +72,13 @@ def activate_menu_option(selection):
     if selection == 1:
         display_calendar()
     elif selection == 2:
-        add_new_event()
+        pass
     elif selection == 84:
-        get_year_from_user()
+        get_date_from_user("start")
     elif selection == 85:
-        get_month_from_user()
-    elif selection == 87:
         get_time_from_user()
-    elif selection == 88:
-        add_test_event()
-        
+    elif selection == 86:
+        pass
     else:
         pass
 
@@ -94,59 +99,42 @@ def display_calendar():
     input("\nPress any key to conrinue")
 
 
-def get_year_from_user():
+def get_date_from_user(start_or_end):
     """
-    Get year from user
-    year must be equal to or higher than current year
-    maximum valid year equals current year + 99 years
+    Get event date from user
+    Displays error message if date is invalid
     """
-    MIN_YEAR = D.today().year  # Min valid year is current year
-    MAX_YEAR = MIN_YEAR + 99  # Max valid year is current year + 100
-    year = 0
+    is_date_valid = False
+    yes_or_no = ""
 
-    while year not in range(MIN_YEAR, MAX_YEAR):
+    while not is_date_valid:
+        yes_or_no = ""  # resets yes_or_no variable
+
         try:
-            year = int(input("Please enter the year of the event "
-                             "in the YYYY format: \n"))
-            if year not in range(MIN_YEAR, MAX_YEAR):
-                print("The year must be equal to or higher than the "
-                      "current year\n")
+            date_str = input(
+                             f"Please enter the {start_or_end} date "
+                             "in MM-DD-YYYY format: \n"
+                             )
+            date_list = date_str.split("-")
+            date_list = [int(string) for string in date_list]
+            chosen_date = datetime.date(date_list[2],
+                                        date_list[1],
+                                        date_list[0])
         except ValueError:
-            print("You must enter a valid number in the YYYY format\n")
+            print("Invalid date")
+        else:
+            print(
+                  f"\nThe date you selected is {date_list[0]} "
+                  f"{month_dictionary[date_list[1]]}, {date_list[2]}"
+                  )
 
-    print(year)
-    input("Press any key to continue")
-    return year
+            while yes_or_no not in ["Y", "y", "n", "N"]:
+                yes_or_no = input("\nIs that correct (Y/N)? \n")
 
+            if yes_or_no in ["Y", "y"]:
+                is_date_valid = True
 
-def get_month_from_user():
-    """
-    Get month from user
-    """
-    month = 0
-
-    while month not in range (1,13):
-        try:
-            print("Event Month: ")
-            print("1 - January")
-            print("2 - February")
-            print("3 - March")
-            print("4 - April")
-            print("5 - May")
-            print("6 - June")
-            print("7 - July")
-            print("8 - August")
-            print("9 - September")
-            print("10 - October")
-            print("11 - November")
-            print("12 - December")
-            month = int(input("\nPlease make your selection (1 - 12)\n"))
-
-            if month not in range(1, 13):
-                print("Please choose a number from 1 to 12\n")
-
-        except ValueError:
-                print("Please choose a number from 1 to 12\n")
+    return chosen_date
 
 
 def get_time_from_user():
@@ -155,7 +143,7 @@ def get_time_from_user():
     """
 
     try:
-        time_str = input("Please enter the event time in HH:MM format:\n")
+        time_str = input("Please enter the event time in HH:MM 24h format:\n")
         time_str = time_str.split(":")
         print(time_str)
         print(len(time_str))
@@ -174,23 +162,25 @@ def get_time_from_user():
     return event__time
 
 
-        
-            
 
 
-    print(month)
-    input("Press any key to continue")
-    return month
 
-
-def add_test_event():
+""" def add_test_event():
     event = Event(
-    'Breakfast',
-    start=(22/Feb/2021)[9:00],
-    end=(12/Sept/2020)[9:45],
-    )
+        'Breakfast',
+        start=(1 / Jan / 2019)[9:00],
+        recurrence=[
+            Recurrence.rule(freq=DAILY),
+            Recurrence.exclude_rule(by_week_day=[SU, SA]),
+            Recurrence.exclude_times([
+                (19 / Apr / 2019)[9:00],
+                (22 / Apr / 2019)[9:00]
+            ])
+        ],
+        minutes_before_email_reminder=50
+) """
 
-    CALENDAR.add_event(event)
+
 
 def main():
     """
@@ -210,3 +200,16 @@ print("Welcome to Kalendar")
 print("A Python-based Google Calendar Interface\n")
 print("Calendar URL: https://bit.ly/392Xz9R\n")
 main()
+
+""" print("start print")
+for events in CALENDAR:
+    print(events)
+print("end print") """
+""" event = Event(
+    'Breakfast',
+    start=(18/Sept/2021)[9:00],
+    end=(18/Sept/2021)[9:45],
+    event_id="12345"
+    )
+
+CALENDAR.add_event(event) """
